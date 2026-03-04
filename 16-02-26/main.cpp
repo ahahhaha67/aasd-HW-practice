6e4f0b86f32966add65befe0023d566f155cda23
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-03-04 23:32:52 +0300

    add unique template function

--- 16-02-26/main.cpp
+++ 16-02-26/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
+template <class T>
+T *unique(T *a, size_t s)
+{
+  if (s == 0)
+    return a;
+
+  T *end = a + 1;
+
+  for (size_t i = 1; i < s; ++i)
+  {
+    bool isDuplicate = false;
+    for (T *p = a; p != end; ++p)
+    {
+      if (*p == a[i])
+      {
+        isDuplicate = true;
+        break;
+      }
+    }
+    if (!isDuplicate)
+    {
+      *end = a[i];
+      ++end;
+    }
+  }
+  return end;
+}
+
 int main()
 {
+  return 0;
 }
