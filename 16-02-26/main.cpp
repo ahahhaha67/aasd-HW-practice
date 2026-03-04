01afbf883b05f4b148f73178b65a163e06c57492
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-03-04 23:33:22 +0300

    add int array test

--- 16-02-26/main.cpp
+++ 16-02-26/main.cpp
@@ -30,5 +30,16 @@ T *unique(T *a, size_t s)
 
 int main()
 {
+  int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+  size_t n = sizeof(arr) / sizeof(arr[0]);
+
+  int *newEnd = unique(arr, n);
+  size_t newSize = newEnd - arr;
+
+  std::cout << "Unique integers (" << newSize << "): ";
+  for (int *p = arr; p != newEnd; ++p)
+    std::cout << *p << " ";
+  std::cout << std::endl;
+
   return 0;
 }
